62ad8666bb3dd820ff6a3775c606cba0f5271d0d
Peter <30997029+Supernarioc@users.noreply.github.com>
2018-09-29 15:09:22 +1200

    change server method

--- server.js
+++ server.js
@@ -20,88 +20,30 @@ api.get('/start', (req, res) => {
 });
 
 // Rounds assigned to each truck on a given day
-api.get('/rounds/:date', (req, res) => {
+api.get('/Alllocations/', (req, res) => {
 	let params = req.params.date;
 
-	dbHelper.rounds.get(params).then((result) => {
+	dbHelper.location.getAll(params).then((result) => {
 		res.send(result);
 	});
 });
-
-api.get('/releases/:date', (req, res) => {
-	let params = req.params.date;
-
-	// Query the truncated version
-	dbHelper.releases.get(params).then((result) => {
-		res.send(result.releases);
-	});
-});
-
-// Retrieves all the full releases
-api.get('/full_releases', (req, res) => {
-	// Should retrieve all releases that were started within the past 30 days?
-	dbHelper.fullReleases.get(null, null).then((result) => {
-		res.send(result);
-	})
-});
-
-// This needs to retrieve the whole release given the parameters
-api.get('/full_releases/:releaseID', (req, res) => {
-	// This is bad form need to change it.
-	let params = req.params.releaseID.split('@');
-
-	dbHelper.fullReleases.get(params[0], params[1]).then((result) => {
-		res.send(result);
-	})
-});
-
-// Gets a TruckRounds object from the frontend and will update the database with the new information
-api.post('/update_rounds', (req, res) => {
+// Adds a new location to the location collection 
+api.get('/add_location'),(req,res)=>{
 	console.log(req.body);
-	res.send({result: 200});
-});
-
+	res.sendStatus(200);
+}
 // Gets a Change object from the frontend and will update the truncated version of the releases in the database with the new information
-api.post('/update_release', (req, res) => {
+api.post('/update_location', (req, res) => {
 	console.log(req.body);
+	dbHelper.releases.update(null, null);
+	
 
-	// if (req.body.increase1) {
-	// 	console.log(`Increase ${req.body.increase1.release}`);
-	// 	dbHelper.releases.update(null, null);
-	// }
-	//
-	// if (req.body.increase2) {
-	// 	console.log(`Increase ${req.body.increase2.release}`);
-	// 	dbHelper.releases.update(null, null);
-	// }
-	//
-	// if (req.body.decrease1) {
-	// 	console.log(`Decrease ${req.body.decrease1.release}`);
-	// 	dbHelper.releases.update(null, null);
-	// }
-	//
-	// if (req.body.decrease2) {
-	// 	console.log(`Decrease ${req.body.decrease2.release}`);
-	// 	dbHelper.releases.update(null, null);
-	// }
-
-	res.send({result: 200});
-});
-
-// Adds a new release to the FullReleases collection and then adds a truncated version to the Releases collection
-api.post('/add_release', (req, res) => {
-	console.log(req.body);
 	res.sendStatus(200);
 });
 
 // Need to find and remove the release based on it's ID as given
-api.delete('/delete_release/:release', (req, res) => {
+api.delete('/delete_location/:locationname', (req, res) => {
 	console.log(req.params.release);
-	res.sendStatus(200);
-});
-
-auth.post('/test', (req, res) => {
-	console.log(req.body);
 	res.sendStatus(200);
 });
 
